0360a4356983a0df51c41f65a3228f64f2720950
eduardaczza <165608058+eduardaczza@users.noreply.github.com>
2024-07-15 16:53:38 -0300

    conteudo construtores adicionado

---
+++ PastaConteudos/Conteudos/Carros2.java
@@ -0,0 +1,45 @@
+package PastaConteudos.Conteudos;
+
+public class Carros2 {
+
+    //usando construtores
+    
+    String marca;
+    String modelo;
+    int numPassageiros;
+    double capCombustivel;
+    double consCombst;
+
+
+    Carros2(){
+        System.out.println("Classe Carros2 foi instanciada.");
+        numPassageiros = 4;
+    }
+
+    Carros2(String marca_, String modelo_, int numPassageiros_, double capCombustivel_, double consCombst_){
+        //construtores com parametros
+        marca = marca_;
+        modelo = modelo_;
+        numPassageiros = numPassageiros_;
+        capCombustivel = capCombustivel_;
+        consCombst = consCombst_;
+    }
+
+    void exibirAutonomia() {
+        System.out.println("A autonomia do carro é: " + capCombustivel * consCombst + " Km");
+    }
+
+    double obterAutonomia() { 
+
+        System.out.println("Métodos obterAutonomia foi chamado.");
+
+        return capCombustivel * consCombst;
+    }
+
+    double calcularCombus(double Km){
+        
+        double qtdCombs = Km/consCombst;
+
+        return qtdCombs;
+    }
+}
---
+++ PastaConteudos/Conteudos/TesteC2.java
@@ -0,0 +1,22 @@
+package PastaConteudos.Conteudos;
+
+public class TesteC2 {
+    public static void main(String[] args) {
+        Carros2 van = new Carros2();
+        van.marca = "Fiat";
+        van.modelo = "Ducato";
+        //van.numPassageiros = 10;
+        van.capCombustivel = 100;
+        van.consCombst = 0.2;
+
+        System.out.println(van.numPassageiros);
+
+        Carros2 van2 = new Carros2("Fiat", "Ducato", 10, 100, 0.2);
+        
+        System.out.println(van2.marca);
+        System.out.println(van2.modelo);
+        System.out.println(van2.numPassageiros);
+        System.out.println(van2.capCombustivel);
+        System.out.println(van2.consCombst);
+    }
+}
