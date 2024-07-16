3b0074717dbed8c2b8646c75579b0282a848c19c
eduardaczza <165608058+eduardaczza@users.noreply.github.com>
2024-07-16 18:16:12 -0300

    refeito

---
+++ PastaConteudos/Conteudos/Carro/Carro.java
@@ -0,0 +1,34 @@
+package PastaConteudos.Conteudos.Carro;
+
+public class Carro {
+    
+    String marca;
+    String modelo;
+    int numPassageiros;
+    double capCombustivel;
+    double consCombst;
+
+    //método simples sem retorno
+    void exibirAutonomia() { // declaração de um método, deve começar com um verbo de preferência de acordo
+                             // com as boas praticas
+        System.out.println("A autonomia do carro é: " + capCombustivel * consCombst + " Km");
+    }
+
+    // outro método agora com retorno
+    double obterAutonomia() { // se não colocar o return o java vai exigir que mude o método para void
+
+        System.out.println("Métodos obterAutonomia foi chamado.");
+
+        return capCombustivel * consCombst;
+    }
+
+    //adição de outro método
+    double calcularCombus(double Km){
+        
+        double qtdCombs = Km/consCombst;
+
+        return qtdCombs;
+    }
+
+}
+
---
+++ PastaConteudos/Conteudos/Carro/TesteCarro.java
@@ -0,0 +1,29 @@
+package PastaConteudos.Conteudos.Carro;
+
+public class TesteCarro {
+    public static void main(String[] args) {
+
+        Carro van = new Carro();
+        van.marca = "Fiat";
+        van.modelo = "Ducato";
+        van.numPassageiros = 10;
+        van.capCombustivel = 100;
+        van.consCombst = 0.2;
+
+        System.out.println(van.marca);
+        System.out.println(van.modelo);
+
+        // chamando o metodo
+        van.exibirAutonomia();
+
+        double autonomia = van.obterAutonomia();
+        System.out.println("A autonomia do carro é: " + autonomia);
+        System.out.println("A autonomia do carro é: " + van.obterAutonomia());
+
+        double qtdCombs10 = van.calcularCombus(10);
+        double qtdCombs15 = van.calcularCombus(15);
+
+        System.out.println("QtdCombs10 = " + qtdCombs10);
+        System.out.println("QtdCombs15 = " + qtdCombs15);
+    }
+}
